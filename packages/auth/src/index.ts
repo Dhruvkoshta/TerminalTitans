fb1208cd00ec8d72c1047979977d6eaafd730311
Dhruvkoshta <dhruvkoshta04@gmail.com>
2025-10-26 17:34:44 +0530

    changes to auth url

--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -10,7 +10,11 @@ export const auth = betterAuth<BetterAuthOptions>({
 
 		schema: schema,
 	}),
-	trustedOrigins: [process.env.CORS_ORIGIN || ""],
+	trustedOrigins: [
+		process.env.CORS_ORIGIN || "",
+		"http://localhost:3001",
+		"https://terminal-titans-web.vercel.app"
+	],
 	emailAndPassword: {
 		enabled: true,
 	},
